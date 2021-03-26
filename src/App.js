b9b43e53b08e13ff0eceeb770a725040454f143f
Sherry <Shahryartariq57@gmail.com>
2021-03-27 00:37:57 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import TodoLists from "./TodoLists";
 
 function App() {
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+
+  const itemAdd = (e) => {
+    setInputList(e.target.value);
+  };
+
+  const addListItem = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
+
+  const deleteItems = (id) => {
+    console.log("Deleted - App");
+    setItems((oldItems) => {
+      return oldItems.filter((arrayElement, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <div className="center__div">
+          <br />
+          <h1>ToDo List - Sherry</h1>
+          <br />
+          <div className="main__div">
+            <input
+              type="text"
+              value={inputList}
+              placeholder="Add a Item"
+              onChange={itemAdd}
+            />
+            <button onClick={addListItem}> + </button>
+            <ol>
+              {/* <li>{inputList}</li> */}
+              {items.map((itemValue, index) => {
+                return (
+                  <>
+                    <TodoLists
+                      text={itemValue}
+                      id={index}
+                      key={index + 1}
+                      onSelect={deleteItems}
+                    />
+                  </>
+                );
+              })}
+            </ol>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
